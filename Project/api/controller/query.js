f6a4a547bf7c4cdb07c15b9e2e626e1e75333130
oleks.cherkashyn@gmail.com <oleks.cherkashyn@gmail.com>
2023-10-12 14:02:31 +0300

    db controller v1

---
+++ Project/api/controller/query.js
@@ -0,0 +1,12 @@
+import db from "../db.js"
+
+export const getAllData = (req,res)=>{
+    const q = `Select "Area", "Group","Description" From 'Counters'`
+
+    db.query(q,(err,data)=>{
+        if(err) return res.status(500).send(err);
+
+        return res.status(200).json(data);
+        
+    })
+}
--- Project/client/my-app/src/App.js
+++ Project/client/my-app/src/App.js
@@ -4,20 +4,9 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
