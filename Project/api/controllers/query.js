ab6c39bde0fd0516bd24f1a2d473f8ae5fce028a
oleks.cherkashyn@gmail.com <oleks.cherkashyn@gmail.com>
2024-06-25 09:51:08 +0300

    Error no objects

--- Project/api/controllers/query.js
+++ Project/api/controllers/query.js
@@ -45,6 +45,10 @@ export const getAllData = async (req,res)=>{
 export const getFilterData = async (req,res) =>{
   let newarray = []
   let finalarray = []
+  console.log(req.body.checked[0])
+  if(req.body.checked[0] == undefined){
+    return res.status(404).json("Select objects")
+  }
     const tagArray = splitArray(req.body.checked)
     let array = []
     //check if client insert date filters
